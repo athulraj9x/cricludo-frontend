2c3b88ca051ebb7ab4c1a05eab250874547408ec
abhijith9x <abhijith@9xtechnology.com>
2025-10-18 15:19:12 +0400

    time format changed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,11 +16,20 @@ export const userTypeCollection = {
   "5": "agent"
 } as const;
 
-export function convertSecondsToHHMMSS(seconds: number): string {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const secs = Math.floor(seconds % 60);
-  return `${hours.toString().padStart(2, '0')}:${minutes
-    .toString()
-    .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+export function formatPlaytime(seconds:number): string {
+  if (seconds < 60) {
+    return `${seconds.toFixed(0)} seconds`;
+  } else if (seconds < 3600) {
+    const minutes = Math.floor(seconds / 60);
+    const sec = Math.floor(seconds % 60);
+    return sec > 0
+      ? `${minutes} minute${minutes > 1 ? "s" : ""} ${sec} sec`
+      : `${minutes} minute${minutes > 1 ? "s" : ""}`;
+  } else {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    return minutes > 0
+      ? `${hours} hour${hours > 1 ? "s" : ""} ${minutes} minute${minutes > 1 ? "s" : ""}`
+      : `${hours} hour${hours > 1 ? "s" : ""}`;
+  }
 }
